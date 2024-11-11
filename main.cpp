5262c3b10052e3470170c5f10f241018e55d3ce5
Ivan Guillardini Montes <iguimon@upv.edu.es>
2024-11-11 09:23:02 +0100

    Añadida función main() para probar la clase BrazoRobotico

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// main.cpp
+#include "BrazoRobotico.h"
+#include <iostream>
+
+int main() {
+    BrazoRobotico brazo;
+
+    brazo.mover(5.0, 10.0, 15.0);
+    brazo.coger();
+    brazo.soltar();
+
+    return 0;
+}
+
